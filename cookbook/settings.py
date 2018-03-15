4c0db64822daad05754e0f60780d8db3cab896c5
Jonir Rings <peterpuyi@live.cn>
2018-03-15 18:10:15 +0800

    now it can query

--- cookbook/settings.py
+++ cookbook/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cookbook.ingredients',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+GRAPHENE = {
+    'SCHEMA': 'cookbook.schema.schema'
+}
