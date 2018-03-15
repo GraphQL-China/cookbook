e6563efeea42a7cdff6a6075cd98ff278601d08e
Jonir Rings <peterpuyi@live.cn>
2018-03-15 18:10:35 +0800

    query single object

--- cookbook/ingredients/schema.py
+++ cookbook/ingredients/schema.py
@@ -16,12 +16,43 @@ class IngredientType(DjangoObjectType):
 
 
 class Query(object):
+    category = graphene.Field(CategoryType,
+                              id=graphene.Int(),
+                              name=graphene.String())
     all_categories = graphene.List(CategoryType)
+
+
+    ingredient = graphene.Field(IngredientType,
+                                id=graphene.Int(),
+                                name=graphene.String())
     all_ingredients = graphene.List(IngredientType)
 
     def resolve_all_categories(self, info, **kwargs):
         return Category.objects.all()
 
     def resolve_all_ingredients(self, info, **kwargs):
-        # We can easily optimize query count in the resolve method
-        return Ingredient.objects.select_related('category').all()
+        return Ingredient.objects.all()
+
+    def resolve_category(self, info, **kwargs):
+        id = kwargs.get('id')
+        name = kwargs.get('name')
+
+        if id is not None:
+            return Category.objects.get(pk=id)
+
+        if name is not None:
+            return Category.objects.get(name=name)
+
+        return None
+
+    def resolve_ingredient(self, info, **kwargs):
+        id = kwargs.get('id')
+        name = kwargs.get('name')
+
+        if id is not None:
+            return Ingredient.objects.get(pk=id)
+
+        if name is not None:
+            return Ingredient.objects.get(name=name)
+
+        return None
